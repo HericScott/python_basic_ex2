93e636f31d433ada413562eef051970fc5176a15
scott <hericscott033@gmail.com>
2026-05-29 08:11:01 +0100

    data type and example

---
+++ identify_data_type.py
@@ -0,0 +1,38 @@
+#Identify the data type or class for each of the following values:
+
+    #solution
+'True'
+print(type(True))
+
+'False'
+print(type(False))
+
+(1, 2, 3)
+print(type((1, 2, 3)))
+
+1.5
+print(type(1.5))
+
+[1, 2, 3]
+print(type([1, 2, 3]))
+
+2
+print(type(2))
+
+range(5)
+print(type(range(5)))
+
+{1, 2, 3}
+print(type({1, 2, 3}))
+
+None
+print(type(None))
+
+{'foo': 'bar'}
+print(type({'foo': 'bar'}))
+
+
+
+
+
+
